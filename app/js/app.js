df16b3251333b6ae91b120bee325cf0c667634e2
MichalVVV <malinka.michal@gmail.com>
2014-06-07 17:52:41 +0200

    implement tags sorting

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,20 @@
 	'use strict';
 	var app = angular.module('myApp', ['onsen.directives', 'google-maps']);
 	
-	app.controller('app', ['$scope', function($scope) {
-		$scope.$root.message = function(mess) {
+	app.controller('app', ['$rootScope', '$scope', function($rootScope, $scope) {
+		$rootScope.message = function(mess) {
 			alert(mess);
 		};
+
+		$rootScope.tags = [{
+			value: 'one',
+			name: 'ONE',
+			checked: false
+		}, {
+			value: 'two',
+			name: 'TWO',
+			checked: false
+		}];
 		
 		$scope.map = {
 			center: {
@@ -16,27 +26,81 @@
 			pan: 'y'
 		};
 		
-		$scope.marker = {
-			coords: {
+		$scope.materials = [];
+
+		$scope.materials = [{
+			location: {
 				latitude: 51.133253392421864,
 				longitude: 17.02015246582033
 			},
-			events: {
-				click: function() {
-					$scope.marker.showWindow = true;
-					//alert('click event');
-				}
+			tags: ['one'],
+			comment: 'Here we were on saturday',
+			photo: 'http://ecx.images-amazon.com/images/I/311KHS%2BIefL._SX270_.jpg'
+		}, {
+			location: {
+				latitude: 51.113253392421864,
+				longitude: 17.02015246582033
 			},
-			showWindow: false/*,
-			labelContent: 'window1.html'*/
-		};
+			tags: ['one', 'two'],
+			comment: 'Here we were on sunday',
+			photo: 'http://images.prd.mris.com/image/V2/1/Yc5MhpzB9jJ6NYHPaRThj8UBi-5Cler_H3qMg4YY3fyM9vW-BGSV7qKsbRcSab-Ed1MOQl4KLDLI4QzXZuboSQ.jpg'
+		}, {
+			location: {
+				latitude: 51.113253392421864,
+				longitude: 17.01015246582033
+			},
+			tags: ['two'],
+			comment: 'Here we were on sunday',
+			photo: 'http://images.prd.mris.com/image/V2/1/Yc5MhpzB9jJ6NYHPaRThj8UBi-5Cler_H3qMg4YY3fyM9vW-BGSV7qKsbRcSab-Ed1MOQl4KLDLI4QzXZuboSQ.jpg'
+		}];
 		
-		$scope.window1 = {
-			isIconVisibleOnClick: false,
-			templateUrl: 'window1.html',
-			closeClick: function() {
-				alert('close');
+		$scope.globalEvents = {
+			click: function(a, b, c) {
+				this.$parent.marker.showWindow = true;
 			}
+		};
+		/*
+		$scope.$watch('$root.tags', function() {
+			console.log($rootScope.tags);
+			var tags = $rootScope.tags.sort();
+			var toReturn = [];
+			angular.forEach($scope.materialsAll, function(material) {
+				var materialTags = material.tags;//.sort();
+				var isOk = false;
+				for(var i in tags) {
+					for(var j in materialTags) {
+						if (tags[i].checked && tags[i].value === materialTags[j]) {
+							isOk = true;
+						}
+					}
+				}
+				if (isOk) {
+					toReturn.push(material);
+				}
+			});
+			$scope.materials = toReturn;
+		}, true);*/
+	}]);
+	
+	app.filter('tagowanie', ['$rootScope', function($rootScope) {
+		var tags = $rootScope.tags.sort();
+		return function(materials) {
+			var toReturn = [];
+			angular.forEach(materials, function(material) {
+				var materialTags = material.tags;//.sort();
+				var isOk = false;
+				for(var i in tags) {
+					for(var j in materialTags) {
+						if (((tags[i].checked || tags[i].checked === 'true') && tags[i].checked !== 'false') && tags[i].value === materialTags[j]) {
+							isOk = true;
+						}
+					}
+				}
+				if (isOk) {
+					toReturn.push(material);
+				}
+			});
+			return toReturn;
 		};
 	}]);
 })();
